97f3f0a693fe018e371cef1742bee078ba2ae5c6
ronger <ronger-x@outlook.com>
2025-02-16 19:12:09 +0800

    :sparkles: 文件上传

---
+++ src/main/java/com/rymcu/mortise/mapper/FilePartDetailMapper.java
@@ -0,0 +1,14 @@
+package com.rymcu.mortise.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.rymcu.mortise.entity.FilePartDetail;
+
+/**
+ * Created on 2025/2/16 18:22.
+ *
+ * @author ronger
+ * @email ronger-x@outlook.com
+ * @desc : com.rymcu.mortise.mapper
+ */
+public interface FilePartDetailMapper extends BaseMapper<FilePartDetail> {
+}
